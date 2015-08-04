f4ab53495c37fc2d8fb71f5d3004030c50d47c4f
Tobias Hammerer <tobias.hammerer@rise-world.com>
2015-08-04 10:45:55 +0200

    add development config, switch to java 7

--- hbm-war/src/main/java/at/thammerer/herbarium/spring/configuration/DevelopSecurityConfiguration.java
+++ hbm-war/src/main/java/at/thammerer/herbarium/spring/configuration/DevelopSecurityConfiguration.java
@@ -1,15 +1,14 @@
 package at.thammerer.herbarium.spring.configuration;
 
-import at.thammerer.herbarium.spring.security.CsrfHeaderFilter;
 import at.thammerer.herbarium.spring.security.SecurityUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
@@ -19,10 +18,11 @@ import javax.sql.DataSource;
 /**
  * @author thammerer
  */
+@Profile("develop")
 @Configuration
 @EnableWebSecurity
 //@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class DevelopSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Inject
 	private SecurityUserDetailsService userDetailsService;
@@ -41,11 +41,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http
 			.httpBasic().and()
 			.authorizeRequests()
-			.antMatchers("/index.html", "/views/public/**", "/css/**", "/font-awesome/**","/fonts/**", "/js/**", "/").permitAll().anyRequest()
+			.antMatchers("/**", "/index.html", "/views/public/**", "/css/**", "/font-awesome/**","/fonts/**", "/js/**", "/").permitAll().anyRequest()
 			.authenticated()
 			.and()
-			.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
-			.csrf().csrfTokenRepository(csrfTokenRepository());
+//			.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+//			.csrf().csrfTokenRepository(csrfTokenRepository());
+		.csrf().disable();
 	}
 
 	private CsrfTokenRepository csrfTokenRepository() {
